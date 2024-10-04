2352989e16ce737795d747d1a23b42e6bc2acb72
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-03 20:59:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ from tkinter import Button, Label,Tk,filedialog, ttk, Frame, PhotoImage
 import pygame
 import random
 import mutagen
+
+pygame.mixer.init()
+pygame.mixer.init(frequency=44100)
+cancion_actual =''
+direcion = ''
+
+def abrir_archivo():
+	global direcion, pos, n,cancion_actual
+	pos = 0
+	n = 0
+	direcion = filedialog.askopenfilenames(initialdir ='/', 
+											title='Escoger la cancion(es)', 
+										filetype=(('mp3 files', '*.mp3*'),('All files', '*.*')))
+	n = len(direcion)
+	cancion_actual = direcion[pos]
+
+	nombre_cancion = cancion_actual.split('/')
+	nombre_cancion = nombre_cancion[-1]

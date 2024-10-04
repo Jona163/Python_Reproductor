63c7bcd9d847d44b986f97382f50f7a7fc260095
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-03 20:59:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # Fecha: 03 Octubre 2024
 # Descripción: Código para Reproductor de musica.
 # GitHub: https://github.com/Jona163
+
+
+from tkinter import Button, Label,Tk,filedialog, ttk, Frame, PhotoImage
+import pygame
+import random
+import mutagen
